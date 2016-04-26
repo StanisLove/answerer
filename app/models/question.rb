edd66df38b86911f2ecc72ec3c6a1e3172cd6040
SA <stankra@yandex.ru>
2016-04-26 23:56:05 +0300

    More validations and tests for Question & Answer added

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
-  has_many  :answers
+  has_many  :answers, dependent: :destroy
 
   validates :title, :body, presence: true
   validates_length_of :title, maximum: 140
+  validates_uniqueness_of :title
 end
