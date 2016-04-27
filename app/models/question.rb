a1f71e3132a909164806354a138da662704f3801
SA <stankra@yandex.ru>
2016-04-27 19:28:33 +0300

    Modern style of validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,5 @@ class Question < ActiveRecord::Base
   has_many  :answers, dependent: :destroy
 
   validates :title, :body, presence: true
-  validates_length_of :title, maximum: 140
-  validates_uniqueness_of :title
+  validates :title, length: { maximum: 140 }
 end
