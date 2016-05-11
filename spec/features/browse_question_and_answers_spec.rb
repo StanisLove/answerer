dcdf1f5048b08483c4927d91da1e49caa93dfdf3
SA <stankra@yandex.ru>
2016-05-11 20:25:51 +0300

    Question with out answers should not contain answer(from form) selectors

--- spec/features/browse_question_and_answers_spec.rb
+++ spec/features/browse_question_and_answers_spec.rb
@@ -7,16 +7,24 @@ feature 'User browse question and answers', %q{
 } do
 
   given!(:question) { create(:question) }
-  given!(:answers)  { create_list(:answer, 2) }   
-  given(:user)      { create(:user) }
+  given!(:answers)  { create_list(:answer, 2, question_id: question.id) }   
+  given!(:question_with_out_answer) { create(:question) }
 
   scenario 'User try to browse answers the question' do
-    visit question_answers_path(question)
+    visit question_path(question)
     expect(page).to have_content(question.title)
     expect(page).to have_content(question.body)
     answers.each do |a|
       expect(page).to have_content(a.body)
     end
-    expect(current_path).to eq question_answers_path(question)
+    expect(current_path).to eq question_path(question)
   end
+
+  scenario 'User try to browse question with out answer' do 
+    visit question_path (question_with_out_answer)
+    expect(page).to have_content(question_with_out_answer.title)
+    expect(page).to have_content(question_with_out_answer.body)
+    expect(page).to_not have_css("div.answers > div")
+  end
+    
 end
