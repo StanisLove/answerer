b5940f9339a263f96a8eb9c1ccf717bae7fab697
SA <stankra@yandex.ru>
2016-05-16 18:35:50 +0300

    Add colum is_best to answers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Answer, type: :model do
   it { should have_db_index :question_id }
   it { should belong_to :user }
   it { should have_db_index :user_id }
+  it { should have_db_column(:is_best).
+              of_type(:boolean).
+              with_options(default: false) }
+
   it { should validate_presence_of :body }
   it { should validate_presence_of :question_id }
   it { should validate_presence_of :user_id }
