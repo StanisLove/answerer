a8747d57aed93195918ca7901462d8e217622088
SA <stankra@yandex.ru>
2016-05-17 21:22:20 +0300

    Add answer scope order_by_best

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,8 @@ class Answer < ActiveRecord::Base
 
   validates :body, :question_id, :user_id, presence: true
 
+  scope :order_by_best, -> { order(is_best: :desc) }
+
   def make_best!
     self.toggle!(:is_best)
   end
