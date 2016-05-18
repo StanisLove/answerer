b854bb4a8ebdbf9537040383411116c03dc065f6
SA <stankra@yandex.ru>
2016-05-19 00:27:50 +0300

    Test Answer method make_best!

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  
   it { should belong_to :question }
   it { should have_db_index :question_id }
   it { should belong_to :user }
@@ -12,4 +13,29 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
   it { should validate_presence_of :question_id }
   it { should validate_presence_of :user_id }
+
+  describe "make_best! method" do
+    let(:question)  { create(:question) }
+    let(:answer_one){ create(:answer, question: question) }
+    let(:answer_two){ create(:answer, question: question) }
+
+    it { expect(answer_one.is_best).to eq false }
+    it { expect(answer_two.is_best).to eq false }
+
+    it "makes answer the best" do 
+      answer_one.make_best!
+      answer_one.reload
+      expect(answer_one.is_best).to eq true
+      expect(answer_two.is_best).to eq false
+    end
+
+    it "changes the best answer" do 
+      answer_one.make_best!
+      answer_two.make_best!
+      answer_one.reload
+      answer_two.reload
+      expect(answer_one.is_best).to eq false
+      expect(answer_two.is_best).to eq true
+    end
+  end
 end
