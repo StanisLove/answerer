261692caf78c77d69b3fd43d56f1134c566c4aef
SA <stankra@yandex.ru>
2016-05-20 00:37:53 +0300

    User can add files to answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,6 +14,7 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @answer = @question.answers.build
+    @answer.attachments.build
   end
 
   def create
--- spec/features/answers/add_files_to_answer_spec.rb
+++ spec/features/answers/add_files_to_answer_spec.rb
@@ -15,11 +15,12 @@ feature 'Add files to answer', %q{
     visit question_path(question)
   end
 
-  scenario 'User adds file when create answer' do
+  scenario 'User adds file when create answer', js: true do
     fill_in 'Новый ответ',  with: answer.body
     attach_file 'Файл', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Отправить ответ'
-
+    wait_for_ajax
+    
     within '.answers' do
       expect(page).to have_link 'spec_helper.rb', href: "/uploads/attachment/file/1/spec_helper.rb"
     end
