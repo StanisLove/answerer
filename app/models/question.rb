6ea27b28db9c354747eaa24ad77046e0b709686c
SA <stankra@yandex.ru>
2016-05-19 22:30:04 +0300

    Question accepts nested attributes for attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,6 @@ class Question < ActiveRecord::Base
 
   validates :title, :body, :user_id, presence: true
   validates :title, length: { maximum: 140 }
+
+  accepts_nested_attributes_for :attachments
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,4 +10,6 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of  :body }
   it { should validate_length_of(:title).is_at_most(140) }
   it { should validate_presence_of  :user_id }
+
+  it { should accept_nested_attributes_for :attachments }
 end
