47f638e6188e6772ca3157353f9ff6190c198271
SA <stankra@yandex.ru>
2016-05-20 22:33:52 +0300

    Add several files to question and answer via gem "cocoon"

--- spec/features/answers/add_files_to_answer_spec.rb
+++ spec/features/answers/add_files_to_answer_spec.rb
@@ -15,15 +15,43 @@ feature 'Add files to answer', %q{
     visit question_path(question)
   end
 
-  scenario 'User adds file when create answer', js: true do
+  scenario 'User adds files when create answer', js: true do
+    within '.answers' do
+      expect(page).to_not have_content 'Файлы'
+    end
+
     fill_in 'Новый ответ',  with: answer.body
-    attach_file 'Файл', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Добавить ещё один файл'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+    click_on 'Добавить ещё один файл'
+    within '.attachments .nested-fields:last-child' do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+
     click_on 'Отправить ответ'
     wait_for_ajax
     
     within '.answers' do
+      expect(page).to have_content 'Файлы'
       expect(page).to have_link 'spec_helper.rb', href: "/uploads/attachment/file/1/spec_helper.rb"
+      expect(page).to have_link 'rails_helper.rb', href: "/uploads/attachment/file/2/rails_helper.rb"
     end
   end
 
+  scenario 'User can add and then revmove file while creating an answer', js: true do
+    fill_in 'Новый ответ',  with: answer.body
+    click_on 'Добавить ещё один файл'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    expect(page).to have_content 'Удалить файл'
+
+    click_on 'Удалить файл'
+    click_on 'Отправить ответ'
+    wait_for_ajax
+    
+    within '.answers' do
+      expect(page).to_not have_link 'spec_helper.rb', href: "/uploads/attachment/file/1/spec_helper.rb"
+      expect(page).to_not have_content 'Файлы'
+    end
+  end
 end
--- spec/features/questions/add_files_to_question_spec.rb
+++ spec/features/questions/add_files_to_question_spec.rb
@@ -14,13 +14,47 @@ feature 'Add files to question', %q{
     visit new_question_path
   end
 
-  scenario 'User adds file when asks the question' do
+  scenario 'User adds files when asks the question', js: true do
     fill_in 'Заголовок',  with: question.title
     fill_in 'Вопрос',     with: question.body
-    attach_file 'Файл', "#{Rails.root}/spec/spec_helper.rb"
+
+    click_on 'Добавить ещё один файл'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+    click_on 'Добавить ещё один файл'
+    within '.attachments .nested-fields:last-child' do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
 
     click_on 'Спросить'
 
+    expect(page).to have_content 'Файлы'
     expect(page).to have_link 'spec_helper.rb', href: "/uploads/attachment/file/1/spec_helper.rb"
+    expect(page).to have_link 'rails_helper.rb', href: "/uploads/attachment/file/2/rails_helper.rb"
+  end
+
+  scenario 'User can add and then revmove file while creating an answer', js: true do
+    fill_in 'Заголовок',  with: question.title
+    fill_in 'Вопрос',     with: question.body
+
+    click_on 'Добавить ещё один файл'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    expect(page).to have_content 'Удалить файл'
+
+    click_on 'Удалить файл'
+    click_on 'Спросить'
+    wait_for_ajax
+
+    within '.question' do
+      expect(page).to_not have_link 'spec_helper.rb', href: "/uploads/attachment/file/1/spec_helper.rb"
+      expect(page).to_not have_content 'Файлы'
+    end
+  end
+
+  scenario "User doesn't add file when asks the quesion" do
+    fill_in 'Заголовок',  with: question.title
+    fill_in 'Вопрос',     with: question.body
+    click_on 'Спросить'
+    expect(page).to_not have_content 'Файлы'
   end
 end
