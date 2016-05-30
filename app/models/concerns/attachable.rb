d838988cc96623327f446fb9f3e744da18af445b
SA <stankra@yandex.ru>
2016-05-30 22:24:30 +0300

    Add Attachable concern to models

---
+++ app/models/concerns/attachable.rb
@@ -0,0 +1,11 @@
+module Concerns::Attachable
+  extend ActiveSupport::Concern
+  
+  included do
+    has_many  :attachments, as: :attachable, dependent: :destroy
+
+    accepts_nested_attributes_for :attachments,
+                                  allow_destroy: true,
+                                  reject_if: proc { |a| a['file'].blank? }
+  end
+end
