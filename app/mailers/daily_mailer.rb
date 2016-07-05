0888dbd414b5a1f960ca12d7ac84ba0c00a588fa
SA <stankra@yandex.ru>
2016-07-05 14:36:50 +0300

    Fix mistakes in daily_mailer

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -1,6 +1,6 @@
 class DailyMailer < ApplicationMailer
   def digest(user)
-		@questions = Question.during_last(30.days)
+		@questions = Question.during_last(24.hours)
     mail to: user.email
   end
 end
