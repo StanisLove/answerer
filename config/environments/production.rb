b0268a9378f498ecbcbf09f20638c07a9d5d669a
SA <stankra@yandex.ru>
2016-07-12 17:31:51 +0300

    Add config.action_mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Email
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'http://146.185.132.25' }
 end
