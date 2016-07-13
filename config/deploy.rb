cbb0b3d3c8ddafb1b3e9e47b7547d22572fc979f
SA <stankra@yandex.ru>
2016-07-13 11:51:59 +0300

    Add passenger deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,68 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, 'answerer'
+set :repo_url,    'git@github.com:StanisLove/answerer.git'
+set :deploy_to,   '/home/deployer/answerer'
+set :deploy_user, 'deployer'
+# set :format, :airbrussh
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml',
+                                                 '.env',                'config/private_pub_thin.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log',           'tmp/pids',
+                                               'tmp/cache',     'tmp/sockets',
+                                               'public/system', 'public/uploads')
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
+
+namespace :private_pub do
+  desc 'Start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml start"
+        end
+      end
+    end
+  end
+
+  desc 'Stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml stop"
+        end
+      end
+    end
+  end
+
+  desc 'Restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml restart"
+        end
+      end
+    end
+  end
+end
+
+after 'deploy:restart', 'private_pub:restart'
