8db65a5e82033ceed27290ecf4e204f3230de68a
SA <stankra@yandex.ru>
2016-07-14 14:15:39 +0300

    Cache Question show

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,7 +1,7 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :subscriber, class_name: User, foreign_key: :user_id
-  belongs_to :question
+  belongs_to :question, touch: true
 
   validates :user_id, :question_id, presence: true
   validates :user_id, uniqueness: { scope: :question_id }, case_sensetive: false
